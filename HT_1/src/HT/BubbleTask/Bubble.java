40ad82d1c6d4883d1ae2a99d03ad1b226eceb0b5
Dzmitry Zyznik <zyznikdzmitry@gmail.com>
2020-09-05 14:48:00 +0300

    Updated bubble

--- HT_1/src/HT/BubbleTask/Bubble.java
+++ HT_1/src/HT/BubbleTask/Bubble.java
@@ -4,17 +4,6 @@ public class Bubble {
     public static final double VOLUME = 0.3;
     public String structure;
 
-    public static double getVOLUME() {
-        return VOLUME;
-    }
-
-    public String getStructure() {
-        return structure;
-    }
-
-    public void setStructure(String structure) {
-        this.structure = structure;
-    }
 
 
 
