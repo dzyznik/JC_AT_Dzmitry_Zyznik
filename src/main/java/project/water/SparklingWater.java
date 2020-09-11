70b5a9926829bd30034f47d4e941a94fac9231b3
Dzmitry Zyznik <zyznikdzmitry@gmail.com>
2020-09-12 02:45:16 +0300

    SparklingWater was updated regarding the 3rd task

--- src/main/java/project/water/SparklingWater.java
+++ src/main/java/project/water/SparklingWater.java
@@ -1,65 +1,103 @@
 package main.java.project.water;
 
-
 import main.java.project.bubbles.Bubble;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.concurrent.TimeUnit;
 
+//SparklingWater:
+//------ + наследуется от Water
+//------ + содеждит приватные поля boolean isOpened, Bubble[] bubbles
+//------ + содержит конструктор SparklingWater(), который вызывает внутренний метод isOpened();
+//------ + есть публичный метод void pump(Bubble[] bubbles), который сетает массив из пузырьков в воду
+//------ + есть публичный метод void setOpened(boolean isOpened), который меняет состояние воды на "открытое"
+//------ + есть публичный метод void checkIsOpened(), который в новом потоке проверят состояние воды на "открытость" и в случае, если она открыта запускает метод degas()
+//------ + есть приватный метод degas(), который каждую секунду выпускает по партии пузырьков из рассчета 10 + 5 * температура_воды
+//------ + есть публичный метод boolean isSparkle(), возвращающий true если в воде еще есть пузырьки газа
 public class SparklingWater extends Water {
-    public boolean condition;
-    double value;
-    Bubble[] bubbles;
-    ArrayList<Bubble> bubblesList;
-    boolean warmWater;
+    private boolean isOpened;
+    private Bubble[] bubbles;
+    private boolean warmWater;
 
-
-    public SparklingWater(boolean condition, double value, int t, boolean warmWater) {
-        this.condition = condition;
-        this.value = value;
-        this.temperature = t;
+    public void setWarmWater(boolean warmWater) {
         this.warmWater = warmWater;
     }
 
-
-    public void pump(Bubble[] bubbles) throws InterruptedException {
-        this.bubbles = bubbles;
-//        this.temp = bubbles.length;
-        for (int i = 0; i < bubbles.length; i++) {
-            this.bubbles[i] = new Bubble();
-        }
-        this.bubblesList = new ArrayList<>(Arrays.asList(bubbles));
-        long startTime = System.nanoTime();
-        isOpened();
-        long endTime = System.nanoTime();
-        long duration = (endTime - startTime);
-        System.out.println(TimeUnit.SECONDS.convert(duration, TimeUnit.NANOSECONDS) + "seconds");
+    public SparklingWater(String color, String transparency, String smell, int temperature) {
+        this.setColor(color);
     }
 
-    public void isOpened() throws InterruptedException {
-        int a = 0;
-        while (condition == true && bubblesList.size() > 0) {
-            if (this.warmWater = true && this.temperature < 41) {
-                a = a + 2;
-                if (a >= 60) {
-                    this.temperature = this.temperature + 0.1;
-                    a = 0;
+    public SparklingWater() {
+        isOpened();
+    }
+
+    private void isOpened() {
+    }
+
+    public void pump(Bubble[] bubbles) {
+        this.bubbles = bubbles;
+        for (int i = 0; i < bubbles.length; i++) {
+            this.bubbles[i] = new Bubble("CO2");
+        }
+
+    }
+
+    public void setOpened(boolean isOpened) {
+        this.isOpened = isOpened;
+        checkIsOpened();
+    }
+
+
+    public void checkIsOpened() {
+        Thread thread = new Thread(() -> {
+            while (!isOpened) {
+                System.out.println("Bottle is closed...");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
-            degas();
-            Thread.sleep(2000);
-        }
-        System.out.println("End temperature" + this.temperature);
+            try {
+                {
+                    degas();
+                }
 
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start();
     }
 
-    public void degas() {
-        for (double i = 0; i < ((this.temperature * 5 + 10) * 2) && i < bubblesList.size(); i++) {
-            this.bubblesList.get(bubblesList.size() - 1).boom();
-            this.bubblesList.remove(bubblesList.size() - 1);
+
+    private void degas() throws InterruptedException {
+        int time = 0;
+        while (this.bubbles.length > 0 && this.isOpened) {
+            if (this.warmWater && this.getTemperature() < 41) {
+                time++;
+                if (time > 60) {
+                    int temperature = this.getTemperature();
+                    temperature++;
+                    this.setTemperature(temperature);
+                    System.out.println("Temperature is set to: " + temperature);
+                    time = 0;
+                }
+            }
+            int count = 0;
+            for (double i = 0; i < ((this.getTemperature() * 5 + 10)) && i < this.bubbles.length; i++) {
+                count++;
+                new Bubble("CO2").cramp();
+                this.bubbles = new Bubble[this.bubbles.length - 1];
+            }
+            if (this.bubbles.length > 0) {
+                isSparkle();
+            }
+            System.out.println("Bubbles were pumped in bottle №" + " : " + count);
+            System.out.println("Bubbles left in bottle №" + " : " + this.bubbles.length);
+            Thread.sleep(1000);
         }
     }
 
-
+    public boolean isSparkle() {
+        return true;
+    }
 }
