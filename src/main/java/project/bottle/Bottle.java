4440ee754e151b2abae15b00a9f60c7651e649ba
Dzmitry Zyznik <zyznikdzmitry@gmail.com>
2020-09-12 02:43:10 +0300

    Bottle was updated regarding the 3rd task

--- src/main/java/project/bottle/Bottle.java
+++ src/main/java/project/bottle/Bottle.java
@@ -3,33 +3,71 @@ package main.java.project.bottle;
 import main.java.project.bubbles.Bubble;
 import main.java.project.water.SparklingWater;
 
+
+//Bottle:
+//------+ содеждит приватные поля double volume, SparklingWater water
+//------ + содержит конструктор Bottle(double volume), в котором бутылка заполняется массивом из пузырьков из рассчета 10000 на каждый литр
+//------ + есть публичный метод void open(), который меняет состояние воды в "открытое" (приблизительно, как this.water.setOpened(true);)
+//------ + есть публичный метод void warm(int temperature), который устанавливает температуру воды в бутылке
+//------ + есть публичный метод SparklingWater getWater() возвращающий обьект воды
+//------ + есть публичный метод setWater(SparklingWater water) добавляющий новый обьект воды
 public class Bottle {
-    public double volume;
-    String water;
-    int t;
-    boolean warmWater = false;
 
-    public Bottle(double volume, String water, int t, boolean warmWater){
-        this.volume = volume;
-        this.water = water;
-        this.t = t;
-        this.warmWater = warmWater;
+
+    private double volume;
+    private SparklingWater water;
+
+    public double getVolume() {
+        return volume;
     }
 
-
-    public Bottle(double volume, String water, int t){
+    public Bottle(double volume) {
         this.volume = volume;
-        this.water = water;
-        this.t = t;
-
-    }
-
-    public void open() throws InterruptedException {
-        SparklingWater open = new SparklingWater(true, this.volume, this.t, this.warmWater);
-        System.out.println("Bottle is opened");
         Bubble[] b = new Bubble[(int) (this.volume * 10000)];
-        open.pump(b);
+        water = new SparklingWater();
+        water.pump(b);
+        System.out.println("New bottle is created");
+    }
+
+    public void open() {
+        water.setOpened(true);
+        System.out.println("Bottle is opened");
+    }
+
+    public void closed() {
+        water.setOpened(false);
+        System.out.println("Bottle is closed");
     }
 
 
+    public void warm(int temperature) {
+        water.setTemperature(temperature);
+        System.out.println("Temperature is set to " + temperature);
+    }
+
+    public void water(String color, String transparency, String smell, int temperature) {
+        water.setColor(color);
+        water.setTransparency(transparency);
+        water.setSmell(smell);
+        water.setTemperature(temperature);
+        System.out.println("Color is set to " + color);
+        System.out.println("Transparency is set to " + transparency);
+        System.out.println("Smell is set to " + smell);
+        System.out.println("Temperature is set to " + temperature);
+    }
+
+    public void warmWater(boolean warmWater) {
+        water.setWarmWater(warmWater);
+        if (warmWater) {
+            System.out.println("Bottle is started warming");
+        }
+    }
+
+    public SparklingWater getWater() {
+        return water;
+    }
+
+    public void setWater(SparklingWater water) {
+        this.water = water;
+    }
 }
