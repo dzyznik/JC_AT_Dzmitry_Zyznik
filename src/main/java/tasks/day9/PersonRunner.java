7653c036d2a97a34c8c70a620a3418b788c3f23f
Dzmitry Zyznik <zyznikdzmitry@gmail.com>
2020-10-07 21:05:33 +0300

    Day9 was updated

--- src/main/java/tasks/day9/PersonRunner.java
+++ src/main/java/tasks/day9/PersonRunner.java
@@ -2,7 +2,9 @@ package main.java.tasks.day9;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PersonRunner {
     public static void main(String[] args) {
@@ -14,22 +16,26 @@ public class PersonRunner {
                 new Person("Роман Петрович", 72, Person.Sex.MAN)
         );
 
-        System.out.println(list.stream().filter(x -> x.sex == Person.Sex.WOMEN && x.age >= 18 && x.age <= 55 || x.sex == Person.Sex.MAN && x.age >= 18 && x.age <= 60).count());
-        List<Person> newList = new ArrayList<>();
-        for (Person person : list) {
-            if (person.sex == Person.Sex.WOMEN) {
-                if (person.age >= 18 && person.age <= 55) {
-                    newList.add(person);
-                }
-            }
-            if (person.sex == Person.Sex.MAN) {
-                if (person.age >= 18 && person.age <= 60) {
-                    newList.add(person);
-                }
-            }
-        }
-
-        System.out.println(newList.size());
+//        list.stream().sorted((p,p1) -> {if (p.sex != p1.sex){return p1.sex.compareTo(p1.sex);} return p.age - p1.age;}).peek(p -> System.out.println(p.name)).collect(Collectors.toList());
+//        System.out.println(list.stream().min(Comparator.comparingInt(p->p.age)).get().name);
+//        System.out.println(list.stream().collect(Collectors.toMap(p -> p.hashCode(),p-> p)));
+        list.stream().collect(Collectors.groupingBy(p -> p.sex)).keySet().forEach(System.out::println);
+//        System.out.println(list.stream().filter(x -> x.sex == Person.Sex.WOMEN && x.age >= 18 && x.age <= 55 || x.sex == Person.Sex.MAN && x.age >= 18 && x.age <= 60).count());
+//        List<Person> newList = new ArrayList<>();
+//        for (Person person : list) {
+//            if (person.sex == Person.Sex.WOMEN) {
+//                if (person.age >= 18 && person.age <= 55) {
+//                    newList.add(person);
+//                }
+//            }
+//            if (person.sex == Person.Sex.MAN) {
+//                if (person.age >= 18 && person.age <= 60) {
+//                    newList.add(person);
+//                }
+//            }
+//        }
+//
+//        System.out.println(newList.size());
 
     }
 }
--- src/main/java/tasks/day9/Stream.java
+++ src/main/java/tasks/day9/Stream.java
@@ -8,9 +8,22 @@ public class Stream {
     public static void main(String[] args) {
 
         List<String> list = Arrays.asList("мама", "мыла", "раму", "мама", "чисто");
-        System.out.println(list.stream().anyMatch(x -> x.equals("мама")));
-        System.out.println(list.stream().allMatch(x -> x.contains("м")));
-        System.out.println(list.stream().map(x -> x.concat("м")));
-        System.out.println(Arrays.toString(list.stream().flatMap(s -> Arrays.stream(s.split("а"))).filter(s -> s.length() > 0).toArray(String[]::new)));
+//        System.out.println(list.stream().anyMatch(x -> x.equals("мама")));
+//        System.out.println(list.stream().allMatch(x -> x.contains("м")));
+//        System.out.println(list.stream().map(x -> x.concat("м")));
+//        System.out.println(Arrays.toString(list.stream().flatMap(s -> Arrays.stream(s.split("а"))).filter(s -> s.length() > 0).toArray(String[]::new)));
+//        System.out.println(list.stream().sorted().collect(Collectors.toList()));
+//        System.out.println(list.stream().sorted((a, b) -> -a.compareTo(b)).distinct().collect(Collectors.toList()));
+
+//        System.out.println(list.stream().flatMap(s -> Arrays.stream(s.split(""))).peek(System.out::println).max(String::compareTo).get());
+//        System.out.println(list.stream().flatMap(s -> Arrays.stream(s.split(""))).reduce((s,s1) -> s1+s).get().length());
+//        System.out.println(list.stream().map(String::length).reduce((x,y) -> x + y).get());
+//        System.out.println(list.stream().mapToInt(String::length).sum());
+//        System.out.println(list.stream().map(String::length).filter(s -> s % 2 != 0).reduce((x,y) -> x + y).get());
+//        System.out.println(list.stream().map(String::length).collect(Collectors.summingInt(i -> {if (i % 2 == 0){
+//        return 0;} else return i;})));
+        System.out.println(list.stream().collect(Collectors.joining(":","<p>","</p>")));
+
+        }
+
     }
-}
