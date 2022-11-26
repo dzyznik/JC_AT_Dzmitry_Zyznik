1bb18089cdc3e8ba1196eb067fbdd1f06d15fb5a
dzmitry_zyznik <dzmitry_zyznik@epam.com>
2022-11-26 17:39:52 +0100

    Revert "Revert "task1 task3 updates""
    This reverts commit 6ada3a1f97caefceafbec5379b3a1f49b1ef2b4c.

--- src/main/java/tasks/day1/Task_3.java
+++ src/main/java/tasks/day1/Task_3.java
@@ -8,26 +8,26 @@ public class Task_3 {
         for (int i = 0; i < mass.length; i++) {  //заполнить его случайными элементами используя Random.nextInt(n)
             mass[i] = rand.nextInt(100);
         }
-        for (int i = 0; i < mass.length; i++) {     //пройти по массиву и вывести все элементы
-            System.out.print(mass[i] + " ");
+        for (int k : mass) {     //пройти по массиву и вывести все элементы
+            System.out.print(k + " ");
         }
         System.out.println();
         for (int i = mass.length - 1; i >= 0; i--) {         // вывести все элементы в обратном порядке
             System.out.print(mass[i] + " ");
         }
         System.out.println();
-        for (int i = 0; i < mass.length; i++) {      // каждый элемент умножить на 5 и вывести результат
-            System.out.print(mass[i] * 5 + " ");
+        for (int k : mass) {      // каждый элемент умножить на 5 и вывести результат
+            System.out.print(k * 5 + " ");
         }
         System.out.println();
-        for (int i = 0; i < mass.length; i++) {      // каждый элемент возвести в квадрат и вывести результат
-            System.out.print(Math.pow(mass[i], 2) + " ");
+        for (int k : mass) {      // каждый элемент возвести в квадрат и вывести результат
+            System.out.print(Math.pow(k, 2) + " ");
         }
         System.out.println();
         int min = mass[0];
-        for (int i = 0; i < mass.length; i++) {      // найти минимальный элемент массива и вывести результат
-            if (min > mass[i]) {
-                min = mass[i];
+        for (int k : mass) {      // найти минимальный элемент массива и вывести результат
+            if (min > k) {
+                min = k;
             }
         }
         System.out.print(min);
@@ -54,8 +54,8 @@ public class Task_3 {
                 }
             }
         }
-        for (int i = 0; i < mass.length; i++){
-            System.out.print(mass[i] + " ");
+        for (int j : mass) {
+            System.out.print(j + " ");
         }
 
 
