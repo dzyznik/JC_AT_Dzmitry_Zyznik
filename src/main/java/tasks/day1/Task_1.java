6ada3a1f97caefceafbec5379b3a1f49b1ef2b4c
dzmitry_zyznik <dzmitry_zyznik@epam.com>
2022-11-26 17:34:21 +0100

    Revert "task1 task3 updates"
    This reverts commit 2de33b4fb2948a3f850ae76120688053792363be.
    Im so sorry!

--- src/main/java/tasks/day1/Task_1.java
+++ src/main/java/tasks/day1/Task_1.java
@@ -3,10 +3,10 @@ package main.java.tasks.day1;
 public class Task_1 {
 
         public static void main(String[] args) {
-        int i = 0;
-        while (i < 21) {
-            System.out.print(i + " ");
-            i++;
+//        int i = 0;
+//        while (i < 21) {
+//            System.out.print(i + " ");
+//            i++;
 
           float d = 0.3f + 0.4f;
 
@@ -16,4 +16,4 @@ public class Task_1 {
 
         }
     }
-    }
+//    }//
